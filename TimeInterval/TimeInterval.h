89464adf16c07275a194c2016e5ea5fad5c51ac8
kelvin <kelvin@bulwinkel.com>
2016-07-02 20:23:10 +1000

    Initial Commit

---
+++ TimeInterval/TimeInterval.h
@@ -0,0 +1,19 @@
+//
+//  TimeInterval.h
+//  TimeInterval
+//
+//  Created by Kelvin Bulwinkel on 2/07/2016.
+//  Copyright © 2016 Bulwinkel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TimeInterval.
+FOUNDATION_EXPORT double TimeIntervalVersionNumber;
+
+//! Project version string for TimeInterval.
+FOUNDATION_EXPORT const unsigned char TimeIntervalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TimeInterval/PublicHeader.h>
+
+
